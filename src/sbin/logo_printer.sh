cbd2bbd726b858ccaa0102ff7403e11c61fa1130
yibo <inrgihc@126.com>
2020-09-08 21:57:57 +0800

    支持gpdb-6.10.1版本

--- src/sbin/logo_printer.sh
+++ src/sbin/logo_printer.sh
@@ -9,8 +9,8 @@
 ############################################
 
 # 发布时间设置
-PUBLISH_DATE=2020-05-01
-VERSION_CODE=1.0
+PUBLISH_DATE=2020-09-08
+VERSION_CODE=1.1
 AUTHOR_NAME=tang
 CONTACT_ADDR=inrgihc@126.com
 
@@ -38,7 +38,7 @@ echo ' Write Author  : '$AUTHOR_NAME'  '
 echo ' Contact Email : '$CONTACT_ADDR'  '
 echo ' Copyright @ tang reserved!'
 echo ''
-echo ' Refrence Address: https://gpdb.docs.pivotal.io/6-6/install_guide/prep_os.html'
+echo ' Refrence Address: https://gpdb.docs.pivotal.io/6-10/install_guide/prep_os.html'
 echo ''
 echo ' Install time: '$now_time ' '
 echo ''
